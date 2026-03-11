330b60b96dc5e91511ba94e80c1aa5003dd2a705
urbanaeem <urbanaeem2006@gmail.com>
2026-03-11 13:22:47 +0500

    practice tailwind

---
+++ snake_2nd_time.js
@@ -0,0 +1,44 @@
+let direction = {x: 0, y: 0}
+
+
+// Game Functions
+function main(ctime){
+    window.requestAnimationFrame(main)
+    console.log(ctime)
+
+    console.log(ctime)
+
+    console.log(ctime)
+    
+    console.log(ctime) 
+
+    console.log(ctime)
+
+    console.log(ctime)
+
+    console.log(ctime)
+
+    console.log(ctime)
+
+    console,log()
+
+    console.log()
+
+    console.log("")
+    alert("this is wrong way")
+
+    alert("this is a wrong")
+
+}
+
+
+
+
+
+
+
+
+
+// Main logic starts here
+window.requestAnimationFrame(main)
+
