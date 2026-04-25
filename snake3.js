47be0f1a7c9ce28b4f7598da9abe05f43d81a039
urbanaeem <urbanaeem2006@gmail.com>
2026-04-25 14:08:26 +0500

    calculator create by me

--- snake3.js
+++ snake3.js
@@ -28,9 +28,9 @@ function isCollide(snake){
         }
     }
     // if you bump into the wall
-        if(snake[0].x >= 18 || snake[0].x <=0  && snake[0].y >= 18 || snake[0].y <= 0) {
+        if(snake[0].x >= 18 || snake[0].x <= 0  && snake[0].y >= 18 || snake[0].y <= 0) {
             return true
-        }
+}
     
 }
 function gameEngine(){
