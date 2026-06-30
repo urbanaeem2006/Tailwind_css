1cde638e554a55c0e92c403b7819224cb5a48aa8
urbanaeem <urbanaeem2006@gmail.com>
2026-06-30 21:04:36 +0500

    dog image project

---
+++ dog-image-project.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btn");
+const dogImage = document.querySelector(".dogImage")
+
+btn.addEventListener("click", function(){
+        fetch("https://dog.ceo/api/breeds/image/random")
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    dogImage.src = data.message;
+    console.log(data)
+})
+
+})
